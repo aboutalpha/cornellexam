95a75cefa8ad79ac5d30fb738b811f84fec758de
michaelluoyunhan <michael_luoyunhan@outlook.com>
2024-01-17 17:25:55 -0600

    Change image to img

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,10 @@
-self.__NEXT_FONT_MANIFEST={pages:{},app:{'/Users/yunhanluo/Documents/Work/SDE/cornell exam website/node_modules/next/font/google/target.css?{"path":"app/layout.tsx","import":"Inter","arguments":[{"subsets":["latin"],"variable":"--font-inter","display":"swap"}],"variableName":"inter"}':["static/media/c9a5bc6a7c948fb0-s.p.woff2"]},appUsingSizeAdjust:!0,pagesUsingSizeAdjust:!1};
+self.__NEXT_FONT_MANIFEST={
+  "pages": {},
+  "app": {
+    "/Users/yunhanluo/Documents/Work/SDE/cornell exam website/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\",\"display\":\"swap\"}],\"variableName\":\"inter\"}": [
+      "static/media/c9a5bc6a7c948fb0-s.p.woff2"
+    ]
+  },
+  "appUsingSizeAdjust": true,
+  "pagesUsingSizeAdjust": false
+}
